8ca029cf935a40868a138d678563cf9903d2c865
Robert Sprunk <github@sprunk.me>
2019-03-22 16:54:20 +0100

    Setup.py and twine to publish package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qobuz-api",
+    version="0.0.1",
+    author="Robert Sprunk",
+    author_email="github@sprunk.me",
+    description="An unofficial Qobuz music streaming API client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Whisprin/qobuz-player",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+)
