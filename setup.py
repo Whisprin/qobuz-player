05e25c079f0e0513948249a95d505e9394054670
Robert Sprunk <github@sprunk.me>
2019-03-28 19:05:15 +0100

    Release v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qobuz-api",
-    version="0.0.2",
+    version="0.0.3",
     author="Robert Sprunk",
     author_email="github@sprunk.me",
     description="An unofficial Qobuz music streaming API client",
